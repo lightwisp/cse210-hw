f94b4a09a8939ce08903ece25aa81618cc2d7bad
lightwisp <sayerdt@byuo.edu>
2025-01-15 14:58:05 -0700

    abstract learning activiy week02

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Jr. Dev";
+        job1._startYear = 2019;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Intern";
+        job2._startYear = 2020;
+        job2._endYear = 2025;
+
+        Resume resume = new Resume();
+        resume._jobs.Add(job1); 
+        resume._jobs.Add(job2); 
+
+        resume.ReturnResume();
+        
     }
 }
+
+
